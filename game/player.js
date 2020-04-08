5aab8758fa179738711a9a8232e96a4372992cd5
Joshua Rosenberger <rosenbergerjoshua@web.de>
2020-04-08 13:26:09 +0200

    Moved throwDice function to game

--- game/player.js
+++ game/player.js
@@ -3,13 +3,9 @@ module.exports = class Player {
     constructor(name,  id) {
         this.playerName = name;
         this.playerId = id;
+        this.position = 0
         console.info("Created new Player:" + name + " with id: " + id)
     }
 
-    throwDice()
-	{
-		var dice = 1 + Math.floor(Math.random()*6);
-		this.postion += dice;
-	}
 }
 //exports.Player = Player
