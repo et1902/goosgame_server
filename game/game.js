67140469d3b15a57b21eeae395f7759bda8326ad
Joshua Rosenberger <rosenbergerjoshua@web.de>
2020-04-08 13:26:57 +0200

    forgott to add this to the last commit

--- game/game.js
+++ game/game.js
@@ -4,7 +4,7 @@ module.exports = class game{
 	constructor( id ) {
 		this.gameId = id;
 		this.players = [];
-		this.activeplayer;
+		this.activeplayer = 0;
 		this.created = new Date();
 		this.board = new Board(69);
 		console.info("Created new Game with id: " + id);
@@ -12,7 +12,7 @@ module.exports = class game{
 
 	next()
 	{
-		if( activeplayer == this.players.length() )
+		if( this.activeplayer == this.players.length )
 		{
 			activeplayer = 0;
 		}
@@ -21,6 +21,17 @@ module.exports = class game{
 			++activeplayer;
 		}
 
-		this.players[activeplayer].throwDice();
+		throwDice();
+	}
+
+	throwDice()
+	{
+		var dice = 1 + Math.floor(Math.random()*6);
+		this.players[activeplayer].postion += dice;
+		console.info("Player " + thisplayers[activeplayer].playerName + " roled a " + dice + "and moved to field " + this.players[activeplayer].postion)
+	}
+
+	addPlayer(player) {
+		this.players.push(player);
 	}
 }
