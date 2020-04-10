badc3ea51db6662944fb5905bae619b3d314d74a
Joshua Rosenberger <rosenbergerjoshua@web.de>
2020-04-10 18:05:39 +0200

    Added action logic to player

--- game/player.js
+++ game/player.js
@@ -4,8 +4,27 @@ module.exports = class Player {
         this.playerName = name;
         this.playerId = id;
         this.position = 0
+        this.actionToDo = null;
         console.info("Created new Player:" + name + " with id: " + id)
     }
+
+    isFree(){
+        return (this.actionToDo == null)
+    }
+
+    setAction(action) {
+        if (this.isFree()) {
+            this.actionToDo = action;
+        } else {
+            console.error("Player isnt free! Do your action first")
+        }
+    }
+
+    getAction() {
+        return this.actionToDo;
+    }
+
+
 
 }
 //exports.Player = Player
