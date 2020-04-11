a2bf4ff10e0da8144df69aa1b2cd3a2c0594c8b6
et1902 <43827504+et1902@users.noreply.github.com>
2020-04-11 18:08:17 +0200

    removed log output for better runtime

--- game/gameboard.js
+++ game/gameboard.js
@@ -3,7 +3,6 @@ const GameBoardField = require('./gameboardfield.js');
 module.exports = class gameBoard{
     constructor(size) {
         this.fields = this.generateFields(size);
-        console.log("Created new board with total Fileds: " + this.fields.length);
     }
 
     generateFields(size){
